faa8d619bdb76f042ab0a90b1fa4772ac063cc2f
Maximilian Köhl <mail@koehlma.de>
2024-02-02 12:22:36 +0100

    fix: usage of `sbom_file` parameter

--- recipes/simple-sbom/steps/00-install.sh
+++ recipes/simple-sbom/steps/00-install.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-SBOM_PATH="${RUGPI_PROJECT_DIR}/${RUGPI_PARAM_SBOM_FILE}"
+SBOM_PATH="${RUGPI_PROJECT_DIR}/${RECIPE_PARAM_SBOM_FILE}"
 SBOM_DIR=$(dirname "${SBOM_PATH}")
 
 if [ -d "${SBOM_DIR}" ]; then
     mkdir -p "${SBOM_DIR}"
 fi
 
-dpkg --list > "${RUGPI_PROJECT_DIR}/${RUGPI_PARAM_SBOM_FILE}"
+dpkg --list > "${SBOM_PATH}"
