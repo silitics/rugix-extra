e3de81aafaf49c34b798435b20a5e8005ab5c5b4
reubenmiller <reuben.d.miller@gmail.com>
2025-03-02 10:45:12 +0100

    feat(simple-sbom): write sbom to artifacts dir

--- recipes/simple-sbom/steps/00-install.sh
+++ recipes/simple-sbom/steps/00-install.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-SBOM_PATH="${RUGIX_PROJECT_DIR}${RECIPE_PARAM_SBOM_FILE}"
+SBOM_PATH="${RUGIX_ARTIFACTS_DIR}/${RECIPE_PARAM_SBOM_FILE}"
 SBOM_DIR=$(dirname "${SBOM_PATH}")
 
 if [ ! -d "${SBOM_DIR}" ]; then
