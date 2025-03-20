57a2fa7e26b3d0766af6b284acae8227f488b0b4
Maximilian Köhl <mail@koehlma.de>
2025-03-20 13:38:48 +0100

    fix: parameter name of GitHub user names

--- recipes/github-ssh-keys/steps/00-run.sh
+++ recipes/github-ssh-keys/steps/00-run.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 mkdir -p /root/.ssh
 
-for gh_user in ${RECIPE_PARAM_GITHUB_USERS}; do
+for gh_user in ${RECIPE_PARAM_USERS}; do
     echo "adding SSH keys of user $gh_user"
     ssh_keys=$(curl -fsSL "https://github.com/${gh_user}.keys" ||:)
     if [ -n "$ssh_keys" ]; then
