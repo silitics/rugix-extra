87058f3bf56c2ff5f571656040e6aacb0bce19e4
Maximilian Köhl <mail@koehlma.de>
2025-03-24 20:19:32 +0100

    fix: broken paths in recipe

--- recipes/github-ssh-keys/steps/00-run.sh
+++ recipes/github-ssh-keys/steps/00-run.sh
@@ -9,10 +9,10 @@ for gh_user in ${RECIPE_PARAM_USERS}; do
     echo "adding SSH keys of user $gh_user"
     ssh_keys=$(curl -fsSL "https://github.com/${gh_user}.keys" ||:)
     if [ -n "$ssh_keys" ]; then
-        echo "# GitHub User: ${gh_user}" >> /root/.ssh/authorized_keys
-        echo "$ssh_keys" >> /root/.ssh/authorized_keys
+        echo "# GitHub User: ${gh_user}" >>"${RUGIX_ROOT_DIR}/root/.ssh/authorized_keys"
+        echo "$ssh_keys" >>"${RUGIX_ROOT_DIR}/root/.ssh/authorized_keys"
     fi
 done
 
-chmod -R 600 /root/.ssh
-cat /root/.ssh/authorized_keys
+chmod -R 600 "${RUGIX_ROOT_DIR}/root/.ssh"
+cat "${RUGIX_ROOT_DIR}/root/.ssh/authorized_keys"
